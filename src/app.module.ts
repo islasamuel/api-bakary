11154ec96ed7882795c670f411d2a9a21c2f0360
Jorge Vargas <luisvardez@gmail.com>
2023-07-05 14:10:58 -0600

    Config - Variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV
+          ? `.${process.env.NODE_ENV}.env`
+          : '.development.env',
+  }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): object {
+    return {
+      NODE_ENV: process.env.NODE_ENV,
+      DB_HOST: process.env.DB_HOST,
+      DB_PORT: process.env.DB_PORT,
+      DB_USER: process.env.DB_USER,
+      DB_PASSWORD: process.env.DB_PASSWORD,
+      DB_DATABASE: process.env.DB_DATABASE,
+    }
+    
+    //return 'Hello World!';
   }
 }
